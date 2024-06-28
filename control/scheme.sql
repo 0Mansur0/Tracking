5307a44bafc03f282081addbd7beb2e5bb373118
TheHumaun <mansurmuratbeko.@gmail.com>
2024-06-28 18:11:43 +0500

    Fixed the scheme and updated the database

--- control/scheme.sql
+++ control/scheme.sql
@@ -1,24 +1,32 @@
 -- NOT READY FOR PRODUCTION
 
+-- types of activities
 CREATE TABLE activities(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date TEXT,
-    person_id INTEGER,
-    start_second INTEGER,
-    end_time_min INTEGER,
-    FOREIGN KEY(person_id) REFERENCES people(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+);
+
+-- consist of activities and people
+CREATE TABLE actions(
+    activity_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    start_timestamp TEXT,
+    duration_seconds INTEGER,
+    FOREIGN KEY(activity_id) REFERENCES activities(id),
+    FOREIGN KEY(person_id) REFERENCES people(id),
+    PRIMARY KEY(activity_id, person_id, start_timestamp)
 );
 
 CREATE TABLE people(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sex_id INTEGER NOT NULL,
     first_name TEXT,
-    middle_name TEXT, -- for russian
+    middle_name TEXT,
     last_name TEXT,
     birth_date TEXT,
     -- parents can be biological or legal
     parent_1_id INTEGER,
-    parent_2_id INTEGER
+    parent_2_id INTEGER,
+    FOREIGN KEY(sex_id) REFERENCES sex(id)
 );
 
 CREATE TABLE sex( -- or gender
